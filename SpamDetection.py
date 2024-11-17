a80cfb3654dc5ca92f8a6d17517e5511fe5c83ab
alexander784 <alexandernyaga1999@gmail.com>
2024-11-17 15:33:52 +0300

    Set the data

--- SpamDetection.py
+++ SpamDetection.py
@@ -3,4 +3,7 @@ import pandas as pd
 
 #data frame
 data = pd.read_csv('/home/alexander/machine/spam/spam.csv')
-print(data.head())
+# Remove duplicates
+data.drop_duplicates(inplace=True)
+data['Category'] = data['Category'].replace(['han','spam'],['Not Spam','Spam'])
+
